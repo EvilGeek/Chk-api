6e0e1a1b33026bdc15246850700edcde2ef883b2
Vaibhav Chandra <vaibhavchandra2007@gmail.com>
2023-03-07 15:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def chk(cc, mon, year, cvv, charge="10"):
 
 #print(chk("4403934457206451", "01", "2027", "864"))
 
-@app.route("/api/v1")
-@app.route("/api/v1/")
+@app.route("/api/v1/check")
+@app.route("/api/v1/check/")
 def v1CheckerAPI():
     sendIP(request, "checker-v1")
     if request.args.get("authKey") not in authKey:
